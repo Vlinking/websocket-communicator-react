cc34eab4cbe8514488558a88ebe726dba38535e1
Vlinking <vlinking@gmail.com>
2017-07-24 00:41:17 +0200

    Install Python 3.5.1 the hard way -- the only way.

--- server.py
+++ server.py
@@ -80,6 +80,6 @@ async def register_handler(websocket, path):
 
 if __name__ == "__main__":
     # our main event loop
-    start_server = websockets.serve(register_handler, 'localhost', 8765)
+    start_server = websockets.serve(register_handler, '0.0.0.0', 8765)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
