e95d2fb6c7424e478f81335a6f42f2ffddc7fc8e
Vlinking <vlinking@gmail.com>
2017-07-23 11:46:03 +0200

    Fix an error when we have no other connection to send to, add docstrings.

--- server.py
+++ server.py
@@ -16,28 +16,49 @@ class AnonymousUser:
     ANON_USERNAME_RANGE_HIGH = 1000000
 
     def __init__(self, connection):
+        """
+        Assign a random string username.
+        """
         self.connection = connection
         self.username = self.ANON_USERNAME_PREFIX + str(random.randint(
             self.ANON_USERNAME_RANGE_LOW,
             self.ANON_USERNAME_RANGE_HIGH
         ))
+        print("User {} connected to server.".format(self.username))
 
     def receive(self):
+        """
+        Facade pattern for connection receive.
+        """
         return self.connection.recv()
 
     def send(self, sender, message):
+        """
+        Facade pattern for connection send.
+        """
         return self.connection.send("{0}: {1}".format(sender.username, message))
 
     def disconnect(self):
+        """
+        Facade pattern for connection close.
+        """
+        print("User {} disconnected from server.".format(self.username))
         return self.connection.close()
 
 
 async def consumer_handler(anon_user):
+    """
+    Asynchronous loop for receiving messages on this connection and sending to all *other* connections.
+    The messages are sent from a single page application client.
+    """
     global connected_users
     while True:
         try:
             message = await anon_user.receive()
-            await asyncio.wait([user.send(anon_user, message) for user in connected_users if anon_user is not user])
+            try:
+                await asyncio.wait([user.send(anon_user, message) for user in connected_users if anon_user is not user])
+            except ValueError:
+                print("Empty coroutine set.")
         except websockets.exceptions.ConnectionClosed:
             anon_user.disconnect()
             break
@@ -46,6 +67,7 @@ async def consumer_handler(anon_user):
 async def register_handler(websocket, path):
     """
     Register user, wait for messages, finally unregister.
+    Executes once so the inner asynchronous loop is what keeps connection alive.
     """
     global connected_users
     anon_user = AnonymousUser(websocket)
